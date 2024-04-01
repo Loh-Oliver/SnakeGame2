1e638120d3ccde2f1f4e38300bdaccd55645636f
Loh Oliver <oliver.loh.2022@scis.smu.edu.sg>
2024-04-01 14:25:09 +0800

    Game

---
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.snakegame;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.os.Bundle;
+import android.app.Activity;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends Activity {
+
+    // Declare an instance of SnakeEngine
+    SnakeEngine snakeEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the SnakeEngine class
+        snakeEngine = new SnakeEngine(this, size);
+
+        // Make snakeEngine the view of the Activity
+        setContentView(snakeEngine);
+    }
+
+    // Start the thread in snakeEngine
+    @Override
+    protected void onResume() {
+        super.onResume();
+        snakeEngine.resume();
+    }
+
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        snakeEngine.pause();
+    }
+}
