cff93b19eb015dca3587b898ab1283bd78b739b2
Loh Oliver <oliver.loh.2022@scis.smu.edu.sg>
2024-04-06 14:34:54 +0800

    Animation added

--- app/src/main/java/com/example/snakegame/MainActivity.java
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -3,12 +3,16 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.app.Activity;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity implements TrainGame.SnakeDeathListener {
@@ -25,8 +29,31 @@ public class MainActivity extends Activity implements TrainGame.SnakeDeathListen
         // Get the pixel dimensions of the screen
         display = getWindowManager().getDefaultDisplay();
         setContentView(R.layout.activity_main);
+        TextView myText =findViewById(R.id.myText);
+        Animation animation;
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.sample_animation);
+        myText.startAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Log.d("Animation", "Animation started");
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Log.d("Animation", "Animation ended");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                Log.d("Animation", "Animation repeated");
+            }
+        });
 
     }
+
 
 
     public void onSnakeDeath() {
